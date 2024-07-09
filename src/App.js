2ac6cb835c545d8a764d2d0e52fbe38d59070f45
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2024-07-09 03:37:06 +0200

    CHORE: Routing to Product page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
+import Product from './pages/product'
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
         </Routes>
       </Router>
     </div>
