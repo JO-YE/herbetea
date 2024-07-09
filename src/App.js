b0b622f917ad2cbcd17cd5605c8c1956136affb5
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2024-07-09 13:03:00 +0200

    CHORE: adding product details page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
-import Product from './pages/product'
+import Product from './pages/product';
+import Productd from "./pages/productdetails";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/product" element={<Product />} />
+          <Route path="/productdetails" element={<Productd />} />
         </Routes>
       </Router>
     </div>
---
+++ src/pages/productdetails.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Mainheader from "../components/header";
+import Foot from '../components/Footer'
+
+const productdetails = () => {
+  return (
+    <div>
+      <Mainheader />
+      <Foot />
+    </div>
+  )
+}
+
+export default productdetails
