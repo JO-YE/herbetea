a593267620f2d6e74b8bfca492bb9c4f8ccdb5cd
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2024-07-09 21:42:26 +0200

    CHORE: responsiveness of checking out

--- src/pages/checkout.jsx
+++ src/pages/checkout.jsx
@@ -7,9 +7,9 @@ const checkout = () => {
     <div>
       <Mainheader />
       <div className="flex justify-center mt-10">
-        <div className="flex justify-between w-full max-w-[1044px] h-[827px] mb-40">
+        <div className="flex flex-col lg:flex-row justify-between w-full max-w-[1044px] h-auto lg:h-[827px] mb-40 space-y-0 lg:space-y-0 lg:space-x-0">
           {/* Left container */}
-          <div className="w-[672px] h-[827px] bg-amber-100 rounded-tl-[10px] rounded-bl-[10px] p-6">
+          <div className="w-full lg:w-[672px] h-auto lg:h-[827px] bg-amber-100 rounded-[10px] p-6">
             <h2 className="text-neutral-900 text-4xl font-normal font-Quicksand">
               Shopping Cart
             </h2>
@@ -19,7 +19,7 @@ const checkout = () => {
           </div>
 
           {/* Right container */}
-          <div className="w-[672px] h-[827px] bg-amber-100 rounded-tr-[10px] rounded-br-[10px] border-l border-neutral-900 p-6">
+          <div className="w-full lg:w-[672px] h-auto lg:h-[827px] bg-amber-100 rounded-[10px] border-t lg:border-t-0 lg:border-l border-neutral-900 p-6">
             <h2 className="text-neutral-900 text-[32px] font-normal font-Quicksand leading-[105px]">
               Shipping Information
             </h2>
@@ -31,22 +31,24 @@ const checkout = () => {
                   placeholder="Email:          johndoe@gmail.com"
                 />
               </div>
-              <div className="rounded-lg border border-green-900">
+              <div className="space-y-4">
                 <input
                   type="text"
-                  className="w-full h-[59px] border border-green-900 pl-5 bg-transparent"
+                  className="w-full h-[59px] rounded-lg border border-green-900 pl-5 bg-transparent"
                   placeholder="Address:    Apt no, Street"
                 />
-                <input
-                  type="text"
-                  className="w-full h-[59px]  border border-green-900 pl-5 bg-transparent"
-                  placeholder="City:"
-                />
-                <input
-                  type="text"
-                  className="w-full h-[59px] border border-green-900 pl-5 bg-transparent"
-                  placeholder="Country:"
-                />
+                <div className="flex flex-col md:flex-row md:space-x-4">
+                  <input
+                    type="text"
+                    className="w-full h-[59px] rounded-lg border border-green-900 pl-5 bg-transparent mb-4 md:mb-0"
+                    placeholder="City:"
+                  />
+                  <input
+                    type="text"
+                    className="w-full h-[59px] rounded-lg border border-green-900 pl-5 bg-transparent"
+                    placeholder="Country:"
+                  />
+                </div>
               </div>
             </form>
 
@@ -57,30 +59,26 @@ const checkout = () => {
               <div>
                 <input
                   type="number"
-                  className="w-full h-[59px] rounded-lg border border-green-900 pl-5 bg-transparent "
+                  className="w-full h-[59px] rounded-lg border border-green-900 pl-5 bg-transparent"
                   placeholder="Card number:    XXXX  XXXX  XXXX  XXXX"
                 />
               </div>
-              <div className="flex space-x-0">
-                <div className="flex-1">
-                  <input
-                    type="text"
-                    className="w-full h-[59px] rounded-lg border border-green-900 pl-5 bg-transparent"
-                    placeholder="Expiry date: MM/YY"
-                  />
-                </div>
-                <div className="flex-1">
-                  <input
-                    type="number"
-                    className="w-full h-[59px] rounded-lg border border-green-900 pl-5 bg-transparent"
-                    placeholder="CVV: XXX"
-                  />
-                </div>
+              <div className="flex flex-col md:flex-row md:space-x-4">
+                <input
+                  type="text"
+                  className="w-full h-[59px] rounded-lg border border-green-900 pl-5 bg-transparent mb-4 md:mb-0"
+                  placeholder="Expiry date: MM/YY"
+                />
+                <input
+                  type="number"
+                  className="w-full h-[59px] rounded-lg border border-green-900 pl-5 bg-transparent"
+                  placeholder="CVV: XXX"
+                />
               </div>
             </form>
 
             <div className="flex justify-center mt-8">
-              <button className="w-full py-3 bg-green-900  text-neutral-50 text-xl font-semibold rounded-lg font-Quicksand">
+              <button className="w-full py-3 bg-green-900 text-neutral-50 text-xl font-semibold rounded-lg font-Quicksand">
                 Place Order
               </button>
             </div>
