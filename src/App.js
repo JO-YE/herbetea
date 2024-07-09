4428d220537dc426818e981bb0e28788ab53b218
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2024-07-09 21:34:57 +0200

    CHORE: adding product details and checking out page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./components/Home";
 import Product from './pages/product';
 import Productd from "./pages/productdetails";
+import Checkout from './pages/checkout'
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/product" element={<Product />} />
           <Route path="/productdetails" element={<Productd />} />
+          <Route path="/checkout" element={<Checkout />} />
         </Routes>
       </Router>
     </div>
