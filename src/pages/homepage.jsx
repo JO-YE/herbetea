3db440ad602c0df0c6adc380b49a3a82fdd784ba
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2024-07-09 03:39:13 +0200

    FIX: Aligning the rows

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -10,14 +10,13 @@ import google from "../components/assets/google.png";
 
 const homepage = () => {
   return (
-    <div className="bg-neutral-50">
-      {/** row 1 */}
-      <div className="relative pt-5 w-[1100px] h-[500px] mx-20 rounded-tl-[10px] rounded-bl-[10px] overflow-hidden">
+    <div className="bg-neutral-50 pt-2">
+      <div className="relative w-full max-w-[1100px] h-[500px] mx-auto rounded-tl-[10px] rounded-bl-[10px] overflow-hidden">
         <div className="absolute inset-0 bg-green-900 opacity-60"></div>
         <div className="relative flex h-full">
           <div className="flex-1 flex items-center justify-center">
             {/* Left side content */}
-            <div className="text-white  ml-20">
+            <div className="text-white ml-20">
               <h2 className="w-[471px] text-neutral-50 text-7xl font-normal font-Playfair Display leading-[80px] mt-0 pt-0">
                 Women’s Herbal Tea
               </h2>
@@ -40,9 +39,9 @@ const homepage = () => {
         </div>
       </div>
       {/** explore categories */}
-      <div className="flex items-center justify-center  my-20">
-        <div className="text-center ">
-          <div className="w-[371px] h-[0px] border border-neutral-900 opacity-90  mb-4"></div>
+      <div className="flex items-center justify-center my-20">
+        <div className="text-center">
+          <div className="w-[371px] h-[0px] border border-neutral-900 opacity-90 mb-4"></div>
           <h3 className="text-2xl font-semibold mb-4">Explore Categories</h3>
           <div className="w-[371px] h-[0px] border border-neutral-900 opacity-90"></div>
         </div>
@@ -50,7 +49,7 @@ const homepage = () => {
 
       {/** row 2 */}
       <div className="flex justify-center mt-5">
-        <div className="relative w-[1100px] h-[500px] rounded-tl-[10px] rounded-bl-[10px] overflow-hidden">
+        <div className="relative w-full max-w-[1100px] h-[500px] rounded-tl-[10px] rounded-bl-[10px] overflow-hidden">
           <div className="absolute inset-0 bg-yellow-500 opacity-25"></div>
           <div className="relative flex h-full">
             <div className="flex-1 relative">
@@ -60,10 +59,10 @@ const homepage = () => {
                 className="w-full h-full object-cover"
               />
             </div>
-            <div className="flex-1 flex items-center justify-center ">
+            <div className="flex-1 flex items-center justify-center">
               {/* Right side content */}
-              <div className=" ml-20">
-                <h2 className="w-[410.66px] h-10 text-neutral-900 text-4xl font-normal font-['Playfair Display']">
+              <div className="ml-20">
+                <h2 className="w-[410.66px] text-neutral-900 text-4xl font-normal font-['Playfair Display']">
                   Slimming Tea
                 </h2>
                 <p className="w-[361px] text-neutral-900 text-1xl mt-5 font-normal font-['Quicksand'] leading-normal">
@@ -80,20 +79,20 @@ const homepage = () => {
       </div>
 
       {/** row 3 */}
-      <div className="relative mt-8 w-[1100px] h-[500px] mx-20 rounded-tl-[10px] rounded-bl-[10px] overflow-hidden">
+      <div className="relative mt-8 w-full max-w-[1100px] h-[500px] mx-auto rounded-tl-[10px] rounded-bl-[10px] overflow-hidden">
         <div className="absolute inset-0 bg-yellow-500 opacity-25"></div>
         <div className="relative flex h-full">
           <div className="flex-1 flex items-center justify-center">
             {/* Left side content */}
-            <div className=" ml-20">
-              <h2 className="w-[410.66px] h-10 text-neutral-900 text-4xl font-normal font-['Playfair Display']">
+            <div className="ml-20">
+              <h2 className="w-[410.66px] text-neutral-900 text-4xl font-normal font-['Playfair Display']">
                 Cleansing Tea
               </h2>
               <p className="w-[361px] text-neutral-900 text-1xl mt-5 font-normal font-['Quicksand'] leading-normal">
                 Our Specially crafted slimming herbal tea blends are designed to
                 support women’s health and well-being.
               </p>
-              <button className=" w-[263px] h-[67px] mt-16 p-2.5 bg-green-900 rounded-lg justify-center items-center gap-2.5 inline-flex text-neutral-50 text-1xl font-semibold font-Quicksand">
+              <button className="w-[263px] h-[67px] mt-16 p-2.5 bg-green-900 rounded-lg justify-center items-center gap-2.5 inline-flex text-neutral-50 text-1xl font-semibold font-Quicksand">
                 Buy Now
               </button>
             </div>
@@ -110,7 +109,7 @@ const homepage = () => {
 
       {/** row 4 */}
       <div className="flex justify-center mt-8">
-        <div className="relative w-[1100px] h-[500px] rounded-tl-[10px] rounded-bl-[10px] overflow-hidden">
+        <div className="relative w-full max-w-[1100px] h-[500px] rounded-tl-[10px] rounded-bl-[10px] overflow-hidden">
           <div className="absolute inset-0 bg-yellow-500 opacity-25"></div>
           <div className="relative flex h-full">
             <div className="flex-1 relative">
@@ -120,10 +119,10 @@ const homepage = () => {
                 className="w-full h-full object-cover"
               />
             </div>
-            <div className="flex-1 flex items-center justify-center ">
+            <div className="flex-1 flex items-center justify-center">
               {/* Right side content */}
-              <div className=" ml-20">
-                <h2 className="w-[410.66px] h-10 text-neutral-900 text-4xl font-normal font-['Playfair Display']">
+              <div className="ml-20">
+                <h2 className="w-[410.66px] text-neutral-900 text-4xl font-normal font-['Playfair Display']">
                   English Tea
                 </h2>
                 <p className="w-[361px] text-neutral-900 text-1xl mt-5 font-normal font-['Quicksand'] leading-normal">
