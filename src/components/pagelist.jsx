5081984f691bf67e5c1d062f255773d8ec788ccd
JO-YE <ainaoluwadamilolaracheal@gmail.com>
2024-07-13 17:02:32 +0200

    CHORE: integrating the API to the product list, product now reads from the API

--- src/components/pagelist.jsx
+++ src/components/pagelist.jsx
@@ -1,54 +1,30 @@
 import React, { useState } from "react";
 import next from "../components/assets/product/nexticon.png";
 
-const PageList = () => {
-  const [selectedPage, setSelectedPage] = useState(1); // this default to page 1
-
-  // this function to handle page click
+const PageList = ({ currentPage, totalPages, onPageChange }) => {
   const handlePageClick = (page) => {
-    setSelectedPage(page);
+    onPageChange(page);
   };
 
-  // working on the change in bg color and text as we click on it
-  const pages = [
-    {
-      id: 1,
-      name: "1",
-      colorClass:
-        selectedPage === 1
-          ? "bg-gray-500 text-white"
-          : "bg-neutral-100 text-gray-500",
-    },
-    {
-      id: 2,
-      name: "2",
-      colorClass:
-        selectedPage === 2
-          ? "bg-gray-500 text-white"
-          : "bg-neutral-100 text-gray-500",
-    },
-    {
-      id: 3,
-      name: "3",
-      colorClass:
-        selectedPage === 3
-          ? "bg-gray-500 text-white"
-          : "bg-neutral-100 text-gray-500",
-    },
-  ];
+  // Generating the pages dynamically based on total pages
+  const pages = [...Array(totalPages).keys()].map((num) => num + 1);
 
   return (
     <div className="flex space-x-4 items-center justify-center pt-10 pb-48">
       {pages.map((page) => (
         <div
-          key={page.id}
-          className={`cursor-pointer rounded-full py-2 px-4 font-semibold font-Playfair Display ${page.colorClass} opacity-100`}
-          onClick={() => handlePageClick(page.id)}
+          key={page}
+          className={`cursor-pointer rounded-full py-2 px-4 font-semibold font-Playfair Display ${currentPage === page ? 'bg-gray-500 text-white' : 'bg-neutral-100 text-gray-500'} opacity-100`}
+          onClick={() => handlePageClick(page)}
         >
-          {page.name}
+          {page}
         </div>
       ))}
-      <button className="w-[30px] h-[30px] relative">
+      <button 
+        className="w-[30px] h-[30px] relative"
+        onClick={() => handlePageClick(currentPage < totalPages ? currentPage + 1 : currentPage)}
+        disabled={currentPage >= totalPages}
+      >
         <img
           src={next}
           alt=""
